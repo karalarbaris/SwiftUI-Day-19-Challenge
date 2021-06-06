811d00d4fe897d3d1fd920f2d11cfff404b34c04
Baris <karalarbaris@gmail.com>
2021-06-06 22:38:16 +0200

    Initial Commit

---
+++ SwiftUI-Day-19-Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Day-19-Challenge
+//
+//  Created by Baris Karalar on 6.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Day-19-Challenge/SwiftUI_Day_19_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Day_19_ChallengeApp.swift
+//  SwiftUI-Day-19-Challenge
+//
+//  Created by Baris Karalar on 6.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Day_19_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

5912fef572f8c06853291babe8a1c2f8e5970db7
Baris <karalarbaris@gmail.com>
2021-06-06 23:46:35 +0200

    You need to build an app that handles unit conversions: users will select an input unit and an output unit, then enter a value, and see the output of the conversion.

--- SwiftUI-Day-19-Challenge/ContentView.swift
+++ SwiftUI-Day-19-Challenge/ContentView.swift
@@ -8,10 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State private var valueToBeConverted = ""
+    @State private var inputType = ""
+    @State private var outputType = ""
+    
+    let lenghtTypes = ["meters", "kilometers", "feet", "yards", "miles"]
+    
+    var result: Double {
+        
+        var length: Measurement<UnitLength>
+        
+        switch inputType {
+        case "meters":
+            length = Measurement(value: Double(valueToBeConverted) ?? 0, unit: UnitLength.meters)
+        case "kilometers":
+            length = Measurement(value: Double(valueToBeConverted) ?? 0, unit: UnitLength.kilometers)
+        case "feet":
+            length = Measurement(value: Double(valueToBeConverted) ?? 0, unit: UnitLength.feet)
+        default:
+            length = Measurement(value: Double(valueToBeConverted) ?? 0, unit: UnitLength.meters)
+        }
+        
+        switch outputType {
+        case "meters":
+            return length.converted(to: UnitLength.meters).value
+        case "kilometers":
+            return length.converted(to: UnitLength.kilometers).value
+        case "feet":
+            return length.converted(to: UnitLength.feet).value
+        default:
+            return 0
+        }
+        
     }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Enter value", text: $valueToBeConverted)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Input type", selection: $inputType) {
+                    ForEach(lenghtTypes, id: \.self) { type in
+                        Text("\(type)")
+                    }
+                }
+
+                Picker("Output type", selection: $outputType) {
+                    ForEach(lenghtTypes, id: \.self) { type in
+                        Text("\(type)")
+                    }
+                }
+                
+                Text("Result: \(result, specifier: "%.2f")")
+            }
+        }
+        
+        
+        
+       
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
